29a10aa9c8b8d2eccaf6fdf66089fc58a4cd9fbb
benJ <26485097+Mil00Z@users.noreply.github.com>
2025-02-13 17:56:29 +0100

    create Page counter + Component counter

---
+++ src/app/counter/page.tsx
@@ -0,0 +1,28 @@
+'use client';
+
+import Image from "next/image";
+import Link from "next/link";
+
+import CounterElement from "@/components/ui/counter/CounterElement";
+
+
+const CounterPage = () => {
+
+  return (
+    <>
+    <section className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
+
+      <div className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
+
+       <CounterElement />
+      
+      </div>
+
+  
+    </section>
+
+    </>
+  )
+}
+
+export default CounterPage
---
+++ src/components/ui/counter/CounterElement.tsx
@@ -0,0 +1,46 @@
+'use client';
+
+import { useEffect, useState } from "react";
+
+const CounterElement = () => {
+
+  const [count, setCount] = useState(0);
+  const [message, setMessage] = useState('Click');
+  const [showLabel, setShowLabel] = useState(true);
+
+
+
+  useEffect(() => {
+
+    if (count > 5) {
+
+      setShowLabel(false)
+
+      setCount(0)
+      
+      // Wait to action 
+      setTimeout(() => {
+
+        setShowLabel(true)
+
+        setMessage('Click')
+
+      },2500)
+
+    } 
+
+  }, [count]);
+  
+
+
+  return (
+    <>
+      <h1 className="title text-3xl">Counter</h1>
+      <div className="counter strike" onClick={()=>setCount(count => count + 1)}>
+          {showLabel ? (<p>{message} : {count} </p>) : (<p>Le compteur est réinitialisé</p>)}
+      </div>
+    </>
+  )
+}
+
+export default CounterElement
