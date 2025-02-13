5259024af5d91024a8b44d0316dfd5ca3d2280d7
benJ <26485097+Mil00Z@users.noreply.github.com>
2025-02-13 12:59:03 +0100

    modifi logic with count Use State + call scss files

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,16 @@
 'use client';
-import { useState, useEffect } from 'react';
+
+import { useEffect, useState } from "react";
 import Image from "next/image";
 import Link from "next/link";
+
+import '../styles/pages/home.scss'
 
 const Home = () => {
 
   const [count, setCount] = useState(0);
-  const [message, setMessage] = useState('');
-  const [showLabel, setShowLabel] = useState(false);
+  const [message, setMessage] = useState('Click');
+  const [showLabel, setShowLabel] = useState(true);
 
 
 
@@ -15,36 +18,39 @@ const Home = () => {
 
     if (count > 5) {
 
-      setCount(0)
+      setShowLabel(false)
 
+      setCount(0)
+      
+      // Wait to action 
       setTimeout(() => {
 
         setShowLabel(true)
-       
-        setMessage('Count')
 
-      },3000)
+        setMessage('Click')
+
+      },2500)
 
     } 
 
   }, [count]);
   
+
+
   return (
+    <>
     <section className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
 
       <div className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
 
         
-      <>
         <h1 className="title text-3xl">Home</h1>
         <div className="counter strike" onClick={()=>setCount(count => count + 1)}>
           {showLabel ? (<p>{message} : {count} </p>) : (<p>Le compteur est réinitialisé</p>)}
           </div>
-      </>
-        
-
+    
+      
       </div>
-
 
       <nav className="flex gap-5 items-end flex-row md:flex-col">
         
@@ -63,7 +69,9 @@ const Home = () => {
       </nav>
 
     </section>
-    
+
+    <div className="home"></div>
+    </>
   )
 }
 
