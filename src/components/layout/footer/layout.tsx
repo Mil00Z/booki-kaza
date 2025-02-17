a678f3ba84e43618840d9073a5216f990b7118e9
benJ <26485097+Mil00Z@users.noreply.github.com>
2025-02-17 16:38:38 +0100

    Footer Image updates

--- src/components/layout/footer/layout.tsx
+++ src/components/layout/footer/layout.tsx
@@ -1,7 +1,7 @@
 
 import Image from 'next/image'
 
-import altLogo from '@public/kasa-alt-logo.svg'
+import altLogo from '@/public/kasa-alt-logo.svg'
 
 import '@/styles/layouts/footer.scss'
 
@@ -9,8 +9,7 @@ import '@/styles/layouts/footer.scss'
 const Footer = () => {
   return (
     <footer id="footer" className="banner-footer">
-      {/* <Image src={altLogo} alt="logo kasa"/> */}
-      <Image src={'../../public/kasa-logo.svg'} alt="logo kasa" width={100} height={100}/>
+      <Image src={altLogo} alt="logo kasa" />
       <p className="copyright">© 2024 Kasa.All rights <a href='https://github.com/Mil00Z/kasa' target='_blank'>reserved</a></p>
     </footer>
   )
