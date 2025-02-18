9a1a7497c992bf3bbd4a369bc4a40893559ec55d
benJ <26485097+Mil00Z@users.noreply.github.com>
2025-02-18 10:03:04 +0100

    debeug call assets from public + allow domains on server

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,9 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
 
+  images: {
+    domains: ['s3-eu-west-1.amazonaws.com'],
+  },
  
 };
 export default nextConfig;
--- src/components/ui/Hero/Hero.tsx
+++ src/components/ui/Hero/Hero.tsx
@@ -1,13 +1,13 @@
-import heroBg from '@/assets/hero-background.jpg'
-import heroOtherBg from '@/assets/hero-annonce-background.jpg'
-import heroAboutBg from '@/assets/hero-about-background.jpg'
+//Get Assets
+import heroBg from '@/public/hero-background.jpg'
+import heroOtherBg from '@/public/hero-annonce-background.jpg'
+import heroAboutBg from '@/public/hero-about-background.jpg'
 
-import '@/styles/components/Hero.scss'
+import 'src/styles/components/Hero.scss'
 
 const HeroBanner = (props) => {
 
   const {layout, details = null} = props
-
 
   //Changing BG in function of layout
   let currentBg;
@@ -29,17 +29,16 @@ const HeroBanner = (props) => {
       currentBg = heroBg
   }
   
-
   return (
+  <>
+    <div className={`hero banner-${layout}`} style={layout === 'about' ? {backgroundImage:`url(${currentBg.src})`} :
+    {backgroundImage:`linear-gradient(to right, rgba(0, 0, 0, var(--op)) 0%, rgba(0, 0, 0, var(--op)) 100%),url(${currentBg.src})`}}>
 
-    <div className={`hero banner-${layout}`} style={layout === 'about' ? {backgroundImage:`url(${currentBg})`} :
-    {backgroundImage:`linear-gradient(to right, rgba(0, 0, 0, var(--op)) 0%, rgba(0, 0, 0, var(--op)) 100%),url(${currentBg})`}}>
-
-      {layout === 'home' ? <h1 className="title" data-page={layout}>Chez vous, partout & ailleurs</h1> : null }
+    {layout === 'home' ? <h1 className="title" data-page={layout}>Chez vous, partout & ailleurs</h1> : null }
 
     </div>
 
+  </>
   )
 }
-
 export default HeroBanner
