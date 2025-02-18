a7d8abecbcd26e011994e1b610f532b2903d095e
benJ <26485097+Mil00Z@users.noreply.github.com>
2025-02-18 14:36:36 +0100

    get data with context and display to create liste of items

--- src/components/ui/Listing/Listing.tsx
+++ src/components/ui/Listing/Listing.tsx
@@ -1,7 +1,7 @@
 "use client";
 
-import { useEffect,useContext } from 'react';
-import { DataContext } from '@/app/layout';
+import { useEffect, useContext } from 'react';
+import { DataContext } from 'src/app/layout';
 
 import Card from '@/components/ui/Card/Card';
 
@@ -11,38 +11,27 @@ import '@/styles/components/Listing.scss'
 
 const Listing = () => {
 
-  const { logements } = useContext(DataContext);
+  const {logements,bookings, setBookings,isBooked, setIsBooked} = useContext(DataContext);
 
-  // const {logements,bookings, setBookings,isBooked, setIsBooked} = useOutletContext();
-  
 
-  // useEffect(() => {
+  useEffect(() => {
     
-  // },[bookings])
+
+
+  },[bookings]);
   
 
   return (
   <>
   <section className="listing-section">
 
-    {/* <h3 className="listing-title"><span className='free-spaces'>{ `${bookings}`}</span> logements encore disponibles ({logements.length})</h3>
+    <h3 className="listing-title"><span className='free-spaces'>{ `${bookings}`}</span> logements encore disponibles ({logements.length})</h3>
 
-      <div className="listing-container">
+    <div className="listing-container">
 
       { logements.map((logement,index) => {
           return (
             <Card key={`${logement.id}-${index}`} logement={logement} bookings={bookings} setBookings={setBookings} isBooked={isBooked} setIsBooked={setIsBooked} />)  
-        })
-      }
-      </div> */}
-
-      <h3 className="listing-title"><span className='free-spaces'>{ `${'Not defined'}`}</span> logements encore disponibles ({logements.length})</h3>
-
-      <div className="listing-container">
-
-      {logements.map((logement,index) => {
-          return (
-            <Card key={`${logement.id}-${index}`} logement={logement} />)  
         })
       }
     </div>
