94fcba11d6df390fa20ae40efaf5353544d4c70f
benJ <26485097+Mil00Z@users.noreply.github.com>
2025-02-19 18:28:30 +0100

    update 6pack component to display datas

--- src/components/ui/SixPack/SixPack.tsx
+++ src/components/ui/SixPack/SixPack.tsx
@@ -1,184 +1,71 @@
+import {useContext} from 'react';
+
+import Link from "next/link";
+import Image from 'next/image'
+
+import { DataContext } from 'src/app/layout';
+
+import '@/styles/components/Card.scss'
 import '@/styles/components/SixPack.scss'
 
 
 const SixPack = () => {
 
+  const {logements} = useContext(DataContext);
 
+  // let randomIndex = Math.floor(Math.random()* 10);
+
+  let sortedLogements = logements.slice(0,6);
+
+ 
   return (
-    <section id="hebergements" className="block-data debeug-alt">
+    <section id="hebergements" className="block-data">
     <h2 className="section-title">
       Hébergements à <span className="hebergements_geoloc">Paris</span>
     </h2>
     <div className="lodges-cards" aria-label="liste des biens à louer">
-      <a href="#">
-        <article className="card card-column">
-          <img
-            src="./images/hebergements/fred-kleber.jpg"
-            alt="Image de la chambre d'hôtel montrant un lit"
-            className="card-thumbnail"
-          />
-          <div className="card-content">
-            <div className="card-txt">
-              <h3 className="card-title">Hôtel du port</h3>
-              <p className="card-subtitle">
-                Nuit à partir de 52<span className="euro">€</span>
-              </p>
+
+    {sortedLogements.map((logement,index)=>{
+
+        return(
+
+          <Link href={`annonce/${logement.id}`} key={`pack-${index}`}>
+          <article className="card card-column">
+            <Image
+              src={logement.cover}
+              alt={`Image de ${logement.title}`}
+              className="card-thumbnail"
+              width={400}
+              height={400}
+            />
+            <div className="card-content">
+              <div className="card-txt">
+                <h3 className="card-title">{logement.title}</h3>
+                <p className="card-subtitle">
+                  Nuit à partir de <span className="euro">50€</span>
+                </p>
+              </div>
+              <div className="card-rating">
+                Note de {logement.rating} sur 5
+                <i className="fa-xs fa-solid fa-star" aria-hidden="true"></i>
+                <i className="fa-xs fa-solid fa-star" aria-hidden="true"></i>
+                <i className="fa-xs fa-solid fa-star" aria-hidden="true"></i>
+                <i className="fa-xs fa-solid fa-star" aria-hidden="true"></i>
+                <i
+                  className="fa-xs fa-solid fa-star neutral-star"
+                  aria-hidden="true"
+                ></i>
+                <span className="sr-only">Note de 4 sur 5</span>
+              </div>
             </div>
-            <div className="card-rating">
-              <i className="fa-xs fa-solid fa-star" aria-hidden="true"></i>
-              <i className="fa-xs fa-solid fa-star" aria-hidden="true"></i>
-              <i className="fa-xs fa-solid fa-star" aria-hidden="true"></i>
-              <i className="fa-xs fa-solid fa-star" aria-hidden="true"></i>
-              <i
-                className="fa-xs fa-solid fa-star neutral-star"
-                aria-hidden="true"
-              ></i>
-              <span className="sr-only">Note de 4 sur 5</span>
-            </div>
-          </div>
-        </article>
-      </a>
-      <a href="#">
-        <article className="card card-column">
-          <img
-            src="./images/hebergements/febrian-zakaria.jpg"
-            alt="Image de la chambre d'hôtel montrant un lit"
-            className="card-thumbnail"
-          />
-          <div className="card-content">
-            <div className="card-txt">
-              <h3 className="card-title">Hôtel Chez Amina</h3>
-              <p className="card-subtitle">
-                Nuit à partir de 96<span className="euro">€</span>
-              </p>
-            </div>
-            <div className="card-rating">
-              <i className="fa-xs fa-solid fa-star" aria-hidden="true"></i>
-              <i className="fa-xs fa-solid fa-star" aria-hidden="true"></i>
-              <i className="fa-xs fa-solid fa-star" aria-hidden="true"></i>
-              <i className="fa-xs fa-solid fa-star" aria-hidden="true"></i>
-              <i
-                className="fa-xs fa-solid fa-star neutral-star"
-                aria-hidden="true"
-              ></i>
-              <span className="sr-only">Note de 4 sur 5</span>
-            </div>
-          </div>
-        </article>
-      </a>
-      <a href="#">
-        <article className="card card-column">
-          <img
-            src="./images/hebergements/reisetopia.jpg"
-            alt="Image de la chambre d'hôtel montrant un lit"
-            className="card-thumbnail"
-          />
-          <div className="card-content">
-            <div className="card-txt">
-              <h3 className="card-title">Hôtel Les mouettes</h3>
-              <p className="card-subtitle">
-                Nuit à partir de 76<span className="euro">€</span>
-              </p>
-            </div>
-            <div className="card-rating">
-              <i className="fa-xs fa-solid fa-star" aria-hidden="true"></i>
-              <i className="fa-xs fa-solid fa-star" aria-hidden="true"></i>
-              <i className="fa-xs fa-solid fa-star" aria-hidden="true"></i>
-              <i className="fa-xs fa-solid fa-star" aria-hidden="true"></i>
-              <i
-                className="fa-xs fa-solid fa-star neutral-star"
-                aria-hidden="true"
-              ></i>
-              <span className="sr-only">Note de 4 sur 5</span>
-            </div>
-          </div>
-        </article>
-      </a>
-      <a href="#">
-        <article className="card card-column">
-          <img
-            src="./images/hebergements/annie-spratt.jpg"
-            alt="Image de la chambre d'hôtel montrant un lit"
-            className="card-thumbnail"
-          />
-          <div className="card-content">
-            <div className="card-txt">
-              <h3 className="card-title">Hôtel de la mer</h3>
-              <p className="card-subtitle">
-                Nuit à partir de 46<span className="euro">€</span>
-              </p>
-            </div>
-            <div className="card-rating">
-              <i className="fa-xs fa-solid fa-star" aria-hidden="true"></i>
-              <i className="fa-xs fa-solid fa-star" aria-hidden="true"></i>
-              <i className="fa-xs fa-solid fa-star" aria-hidden="true"></i>
-              <i className="fa-xs fa-solid fa-star" aria-hidden="true"></i>
-              <i
-                className="fa-xs fa-solid fa-star neutral-star"
-                aria-hidden="true"
-              ></i>
-              <span className="sr-only">Note de 4 sur 5</span>
-            </div>
-          </div>
-        </article>
-      </a>
-      <a href="#">
-        <article className="card card-column">
-          <img
-            src="./images/hebergements/marcus-loke.jpg"
-            alt="Image de la chambre d'hôtel montrant un lit"
-            className="card-thumbnail"
-          />
-          <div className="card-content">
-            <div className="card-txt">
-              <h3 className="card-title">Auberge La Canebière</h3>
-              <p className="card-subtitle">
-                Nuit à partir de 25<span className="euro">€</span>
-              </p>
-            </div>
-            <div className="card-rating">
-              <i className="fa-xs fa-solid fa-star" aria-hidden="true"></i>
-              <i className="fa-xs fa-solid fa-star" aria-hidden="true"></i>
-              <i className="fa-xs fa-solid fa-star" aria-hidden="true"></i>
-              <i className="fa-xs fa-solid fa-star" aria-hidden="true"></i>
-              <i
-                className="fa-xs fa-solid fa-star neutral-star"
-                aria-hidden="true"
-              ></i>
-              <span className="sr-only">Note de 4 sur 5</span>
-            </div>
-          </div>
-        </article>
-      </a>
-      <a href="#">
-        <article className="card card-column">
-          <img
-            src="./images/hebergements/nicate-lee.jpg"
-            alt="Image de la chambre d'hôtel montrant un lit"
-            className="card-thumbnail"
-          />
-          <div className="card-content">
-            <div className="card-txt">
-              <h3 className="card-title">Auberge Le Panier</h3>
-              <p className="card-subtitle">
-                Nuit à partir de 23<span className="euro">€</span>
-              </p>
-            </div>
-            <div className="card-rating">
-              <i className="fa-xs fa-solid fa-star" aria-hidden="true"></i>
-              <i className="fa-xs fa-solid fa-star" aria-hidden="true"></i>
-              <i className="fa-xs fa-solid fa-star" aria-hidden="true"></i>
-              <i className="fa-xs fa-solid fa-star" aria-hidden="true"></i>
-              <i
-                className="fa-xs fa-solid fa-star neutral-star"
-                aria-hidden="true"
-              ></i>
-              <span className="sr-only">Note de 4 sur 5</span>
-            </div>
-          </div>
-        </article>
-      </a>
-      <a href="#hebergements" className="load-more">Afficher plus 👌</a>
+          </article>
+          </Link>
+      )
+
+    })}
+
+      <Link href={'/annonce'} className="btn btn-cta load-more">Afficher plus 👌</Link>
+
     </div>
   </section>
   )
