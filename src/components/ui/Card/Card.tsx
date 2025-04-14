6fa5ca86aa4c4441bb9c075c16e8d52d457293cc
benJ <26485097+Mil00Z@users.noreply.github.com>
2025-04-14 12:24:33 +0200

    Update Card Component : - import type Logement - create interface

--- src/components/ui/Card/Card.tsx
+++ src/components/ui/Card/Card.tsx
@@ -1,28 +1,30 @@
 import Link from "next/link";
 
+import type { Logement } from "@/types/logement.types";
+
+
 import '@/styles/components/Card.scss'
 
+interface  CardProps {
+  logement: Logement;
+  bookings: number;
+  setBookings: (value: (prev:number) => number) => void;
+  isBooked: boolean;
+  setIsBooked: (value:boolean) => void;
+}
 
-const Card = (props) => {  
-
-  const {logement,bookings,setBookings,isBooked, setIsBooked} = props
-
+const Card = ({logement,bookings,setBookings,isBooked, setIsBooked} : CardProps) => {  
 
   return (
-
 
     <Link href={`/annonce/${logement.id}`} className={`card ${isBooked && 'booked'}`} style={{background:`url(${logement.cover}),linear-gradient(to bottom, #FFF 0%, #FFF 42%, #0A0A0A 100%)`}}><h2 className="card-title">{logement.title}</h2>
     <p className="card-description">{logement.description}</p></Link>
         
-    // <Link to={`/annonce/${logement.id}`} className={`card ${isBooked && 'booked'}`} style={{background:`url(${logement.cover}),linear-gradient(to bottom, #FFF 0%, #FFF 42%, #0A0A0A 100%)`}}><h2 className="card-title">{logement.title}</h2>
-    // <p className="card-description">{logement.description}</p></Link>
-  
   )
 
 }
 
 export default Card
-
 
  // TODO : #Développer le booking State :
 // - Element local "mémoire" du state de chacun des élèments
