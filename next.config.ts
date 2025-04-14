6ecee45539badc4654047723067479ae0c65558a
benJ <26485097+Mil00Z@users.noreply.github.com>
2025-04-14 17:28:55 +0200

    issue for escape entities

--- next.config.ts
+++ next.config.ts
@@ -11,6 +11,10 @@ const nextConfig: NextConfig = {
       },
     ],
   },
+  reactStrictMode: true, // Active une vérification plus stricte (optionnel)
+  compiler: {
+    styledComponents: true, // Peut corriger certains problèmes liés aux entités dans les styles
+  },
  
 };
 export default nextConfig;
